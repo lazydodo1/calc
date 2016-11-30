2001387843662953060c48da321286d0e2a83452
lazydodo1 <lazydodo@163.com>
2016-11-30 17:06:23 +0800

    完成“QT窗口与对话框”课程

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -3,6 +3,10 @@
 #include <qlabel.h>
 #include "qaction.h"
 #include "qmenubar.h"
+#include "qlabel.h"
+#include "qtimer.h"
+#include "qdialog.h"
+#include "dialog.h"
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -17,6 +21,18 @@ MainWindow::MainWindow(QWidget *parent) :
     newMenu->addAction(newaction );
 
     connect(newaction,SIGNAL(triggered(bool)),this,SLOT(saveActionClick()));
+    //状态栏显示一条消息，持续指定时间
+    //statusBar()->showMessage("TestMsg",3000);
+    //状态栏 分栏
+    QLabel *bar1= new QLabel("aaaa");
+    QLabel *bar2= new QLabel("定时5秒消失");
+    statusBar()->addWidget(bar1);
+    statusBar()->addWidget(bar2,1);
+    bar1->show();
+    bar2->show();
+    //定时消失
+    QTimer *timer = new QTimer;
+    timer->singleShot(5000,bar2,SLOT(clear()));
 }
 
 MainWindow::~MainWindow()
@@ -26,5 +42,16 @@ MainWindow::~MainWindow()
 
 void MainWindow::saveActionClick()
 {
-    qApp->quit();
+    //手工定义的对话框
+//    QDialog dialog(this);
+//    QLabel *label=new QLabel("测试放在对话框中的控件",&dialog);
+    //qt预定义的对话框
+    Dialog dialog;
+    //显示对话框
+    //dialog.exec();
+    //显示并处理返回值
+    if(dialog.exec()==QDialog::Accepted)
+    {
+        qApp->quit();
+    }
 }
