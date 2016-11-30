57da1aed69e40795f1e6d2dd30877dffefea839d
lazydodo1 <lazydodo@163.com>
2016-11-30 15:28:11 +0800

    修改好计算器界面，然后练习代码添加菜单和信号槽

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,19 +1,30 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 #include <qlabel.h>
+#include "qaction.h"
+#include "qmenubar.h"
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    QLabel *label=new QLabel(this);
-    label->setText("你好");
-    label->show();
-  //测试qt
+    QAction *newaction=new QAction("Save",this);
+    newaction->setShortcut(QKeySequence::Save);
+    newaction->setToolTip("ToolTip:Save");
+    newaction->setStatusTip("StatusTip:Save");
+    QMenu *newMenu=menuBar()->addMenu("File");
+    newMenu->addAction(newaction );
+
+    connect(newaction,SIGNAL(triggered(bool)),this,SLOT(saveActionClick()));
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::saveActionClick()
+{
+    qApp->quit();
 }
--- mainwindow.h
+++ mainwindow.h
@@ -17,6 +17,9 @@ public:
 
 private:
     Ui::MainWindow *ui;
+
+public slots:
+    void saveActionClick();
 };
 
 #endif // MAINWINDOW_H
